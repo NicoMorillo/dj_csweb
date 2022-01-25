a1c2df0aec3b4dd15e1c4562cc719f1542fe48a3
NicoMorillo <nicolasm.morillomartinez@gmail.com>
2022-01-25 15:11:04 +0000

    deleted PK, and added blank on platform

--- cloudsolutions/app/service/models.py
+++ cloudsolutions/app/service/models.py
@@ -6,11 +6,11 @@ class Platform(models.Model):
     name = models.CharField(max_length=20) 
 
 class Service(models.Model):
-    id = models.CharField(primary_key=True, max_length=10)
+    
     name = models.CharField(max_length=50)
     platform = models.CharField(max_length=20)
     cost_aprox = models.IntegerField()
     model = models.CharField(max_length=15)
     date_origin = models.DateField()
     company = models.ForeignKey(Company, null=True, blank=True, on_delete = models.CASCADE)
-    platform = models.ManyToManyField(Platform)
+    platform = models.ManyToManyField(Platform, blank=True)
