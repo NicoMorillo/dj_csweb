08b798f898d3a2e3e0aca6e33ad1b49b17ba476e
NicoMorillo <nicolasm.morillomartinez@gmail.com>
2022-01-25 13:09:01 +0000

    Added admin models

--- cloudsolutions/app/request/admin.py
+++ cloudsolutions/app/request/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from app.request.models import Company
 
 # Register your models here.
+admin.site.register(Company)
--- cloudsolutions/app/service/admin.py
+++ cloudsolutions/app/service/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app.service.models import Platform, Service
 
 # Register your models here.
+admin.site.register(Platform)
+admin.site.register(Service)
